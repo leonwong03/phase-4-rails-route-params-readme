104500326998174edc53c5787efd994a849d839d
leonwong03 <108467768+leonwong03@users.noreply.github.com>
2022-10-13 11:23:39 -0400

    commit tree 57190730f24d4e32592e2aece16fdae79dcf4d4a

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -5,4 +5,10 @@ class CheesesController < ApplicationController
     render json: cheeses
   end
 
+  def show
+    #find a cheese using the id from the URL
+    #send a JSON response using that cheese object
+    cheese = Cheese.find_by(id: params[:id])
+    render json: cheese
+  end 
 end
